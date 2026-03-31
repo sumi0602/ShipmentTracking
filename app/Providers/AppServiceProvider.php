749a729f0a0c81aa19d9953c6bb5ff6e673c04b5
sumith0602 <sumith0602@gmail.com>
2026-03-31 10:32:30 +0530

    docker deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
